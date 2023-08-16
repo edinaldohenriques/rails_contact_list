149ea7406744f5b5007b61b586737b694047f35c
edinaldohenriques <edinaldohenriques02@gmail.com>
2023-08-15 22:11:14 -0300

    initial Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,13 @@ class StaticPagesController < ApplicationController
         @nome = "Edinaldo"  # O rails utiliza o conceito de convention over configuration, ou seja, utlizamos as convention do ruby, não precisamos
         # configurar tudo, o rails já faz essa configuração. Ex: controller: static_page_controller.rb com a view: static_page
     end
+
+    #Funções Static Page
+    # Funcão sobre
+    def sobre 
+    end
+
+    # Função Contato
+    def contato 
+    end
 end
